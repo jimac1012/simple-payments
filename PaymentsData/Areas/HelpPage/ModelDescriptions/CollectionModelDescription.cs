86f8a53e67ad6b11565fd2b19cb5e0c1d6eb5f0e
Jorell John Macapagal <macapagal.jorelljohn12@gmail.com>
2021-11-24 16:44:29 +0800

    Add new solution.
    Add new solution with new web api project.

---
+++ PaymentsData/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PaymentsData.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PaymentsData/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PaymentsData.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
