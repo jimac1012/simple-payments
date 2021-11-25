5e3aa7fe26fb2a1811f80a8a23935531803b43a5
Jorell John Macapagal <macapagal.jorelljohn12@gmail.com>
2021-11-25 16:17:31 +0800

    Add database tables.
    User, Account and Transaction

---
+++ PaymentsDatabase/dbo/Tables/Account.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity,
+	[UserId] nvarchar(128) not null,
+	[Name] nvarchar(50) null,
+	[Type] nvarchar(20) null,
+	[DateCreated] datetime2 not null default getutcdate(),
+	[Balance] MONEY null
+)
---
+++ PaymentsDatabase/dbo/Tables/Transaction.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Transaction]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[AccountId] INT not null,
+	[Status] nvarchar(20) not null,
+	[Amount] MONEY not null default 0,
+	[TransactionFee] MONEY null default 0,
+	[Note] nvarchar(20) null,
+	[DateCreated] datetime2 not null default getutcdate(),
+	[TransactionDate] datetime2 not null default getutcdate(),
+)
