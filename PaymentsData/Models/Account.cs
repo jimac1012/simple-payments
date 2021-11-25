f519aa9a3bb5ac4e7a7f0a5014c40f433b530d23
Jorell John Macapagal <macapagal.jorelljohn12@gmail.com>
2021-11-25 20:02:23 +0800

    Add models and rename web app

---
+++ PaymentsData/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentsWeb.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
---
+++ PaymentsData/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PaymentsWeb.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int AccountId { get; set; }
+
+        public string Status { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal TransactionFee { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
